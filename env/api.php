4a3051717a11c7d192b282e05114a957eba2b70b
Jason Reed <jrsofty@gmail.com>
2019-12-28 17:16:56 +0100

    Adds no cache headers to the dev.to proxy
    The proxy wasn't always updating the data on the page. This could be
    from a caching problem. Adding cache control headers to the proxy file
    should correct this in the future.

--- env/api.php
+++ env/api.php
@@ -1,7 +1,10 @@
 <?php
 
 	header('Content-Type: application/json; charset=utf-8');
-
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+	
 	$curlHandle = curl_init();
 	curl_setopt($curlHandle, CURLOPT_URL, "https://dev.to/api/articles?username=jrsofty");
 	curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
