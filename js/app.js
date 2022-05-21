8938c5c53bc1d14bb273dbda9f69b16caf74bdec
Jason Reed <jrsofty@gmail.com>
2022-05-21 21:38:17 +0200

    Implements calls for DEV.to as well

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const GET_USER_ARTICLES = `
 query GetUserArticles($page: Int!){
   user(username: "jrsofty") {
@@ -14,17 +13,57 @@ query GetUserArticles($page: Int!){
     }
   }
 }`;
-async function gql(query, variables={}) {
-    const data = await fetch('https://api.hashnode.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query,
-            variables 
-        })
-    });
 
-    return data.json();
-};
+const maxPosts = 3;
+
+async function gql(query, variables = {}) {
+  const data = await fetch("https://api.hashnode.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  return data.json();
+}
+
+async function devto() {
+  const data = await fetch("https://dev.to/api/articles?username=jrsofty");
+  return await data.json();
+}
+
+function buildCard(id, title, content, link) {
+  let cardwrapper = document.createElement("div");
+  cardwrapper.setAttribute("class", "col-md-3 d-flex align-items-stretch");
+
+  let card = document.createElement("div");
+  card.setAttribute("id", id);
+
+  let cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
+
+  let cardTitle = document.createElement("h3");
+  cardTitle.setAttribute("class", "card-title");
+  cardTitle.innerText = title;
+
+  let cardText = document.createElement("p");
+  cardText.setAttribute("class", "card-text");
+  cardText.innerText = content;
+
+  let moreButton = document.createElement("a");
+  moreButton.setAttribute("class", "card-link");
+  moreButton.setAttribute("target", "_blank");
+  moreButton.innerText = "Read More ...";
+  moreButton.href = link;
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardBody.appendChild(moreButton);
+  card.appendChild(cardBody);
+  cardwrapper.appendChild(card);
+  return cardwrapper;
+}
