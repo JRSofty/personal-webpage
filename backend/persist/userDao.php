683d20db07e57104caf0f51de2bc9977db29eab7
Jason Reed <jrsofty@gmail.com>
2025-01-06 11:55:19 +0100

    Implement lookup by username for login

--- backend/persist/userDao.php
+++ backend/persist/userDao.php
@@ -11,6 +11,30 @@
         public function __construct(){
             $this->connect();
         }
+
+        public function getUserByUsername($username){    
+            $query = "SELECT `id`, `username`, `secret`, `last_name`, `first_name`, `email` FROM `users` WHERE `username` = :username";
+            $params = ["username" => $username];
+            $statement = $this->prepareStatement($query);
+            $result = api_response::getResponse(404);
+            try{
+                $statement->execute($params);
+                $row = $statement->fetch(PDO::FETCH_ASSOC);
+                if(is_array($row)){
+                    $result = api_response::getResponse(200);
+                    $result["user"] = $row;
+                }
+            }catch(Exception $e){
+                $result = api_response::getResponse(500);
+                $result["exception"] = $e->getMessage();
+            }finally{
+                $this->disconnect();
+            }
+            if($result["status"] == 404){
+                $result["message"] = "No User Found";
+            }
+            return $result;
+        }
     }
 
 ?>
